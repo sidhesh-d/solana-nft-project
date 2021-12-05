3bcf673354b172e586bd4809741949a20846864a
Sidhesh Deshmukh <sidhesh@curiositygym.com>
2021-12-05 15:11:00 +0530

    fix for missing async

--- app/src/App.js
+++ app/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   }
 
   useEffect(() => {
-    const onLoad = () => {
+    const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
     window.addEventListener('load', onLoad);
