5ecffd7eddb64910723c4e446ec49f9c61066bb7
Sidhesh Deshmukh <sidhesh@curiositygym.com>
2021-12-05 15:44:16 +0530

    added func to connect to phantom wallet

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
-
       if (solana) {
         if(solana.isPhantom) {
-          console.log('Phantom wallet is connected');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log('Phantom wallet is connected ', response.publicKey.toString());
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         console.log('Make sure you have connected phatom wallet');
@@ -23,6 +24,24 @@ const App = () => {
       console.log('Error:', error);
     }
   }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Phantom wallet connected ',
+      response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderConnectWallet = () => (
+    <button className="cta-button connect-wallet-button"
+    onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  )
 
   useEffect(() => {
     const onLoad = async () => {
@@ -38,6 +57,7 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          { !walletAddress && renderConnectWallet() }
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
