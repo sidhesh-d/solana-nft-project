a4ba9a9519c848e25ec2da21a3308f3eca06987c
Sidhesh Deshmukh <sidhesh@curiositygym.com>
2021-12-05 14:42:26 +0530

    added function to check if phantom wallet is connected

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,31 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if(solana.isPhantom) {
+          console.log('Phantom wallet is connected');
+        }
+      } else {
+        console.log('Make sure you have connected phatom wallet');
+      }
+    } catch(error) {
+      console.log('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  });
+
   return (
     <div className="App">
       <div className="container">
